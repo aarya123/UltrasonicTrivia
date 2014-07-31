8c78eb5d6384a8bd41b9791e53bb0f0c8d085b6b
Edwin Zhang <edwin.zhang@macboxf0fh04.corp.hulu.com>
2014-07-31 15:29:23 -0700

    Error handling and fixing float conversion

--- Sonic/app.py
+++ Sonic/app.py
@@ -16,8 +16,14 @@ def health_check():
 @app.route('/question/<frequency>')
 def question(frequency):
     session = DBSession()
-    question = session.query(Question).filter(Question.frequency > int(frequency)-2, Question.frequency < int(frequency) + 2).first()
+    question = session.query(Question).filter(Question.frequency > float(frequency)-2, Question.frequency < float(frequency) + 2).first()
+    if question is None:
+        return jsonify(error="No question found")
     question_text = question.question
     choices = [answer.answer for answer in question.answers]
-    answer = [index for index,answer in enumerate(question.answers) if answer.correct].pop()
+    answer = [index for index,answer in enumerate(question.answers) if answer.correct]
+    if len(answer) == 1:
+        answer = answer.pop()
+    else:
+        return jsonify(error="Expected one correct answer but got %d." % len(answer))
     return jsonify(question=question_text, choices=choices, answer=answer)
