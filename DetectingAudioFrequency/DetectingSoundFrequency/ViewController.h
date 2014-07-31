3fef5227caf087b917d2da4f6655990b0d940430
Anubhaw Arya <anubhaw.arya@hulu.com>
2014-07-31 13:56:16 -0700

    Removed storyboard

--- DetectingAudioFrequency/DetectingSoundFrequency/ViewController.h
+++ DetectingAudioFrequency/DetectingSoundFrequency/ViewController.h
@@ -1,10 +1,11 @@
-
-
 #import <UIKit/UIKit.h>
-
+#import "mo_audio.h"
+#import "FFTHelper.h"
 @interface ViewController : UIViewController {
-    
-    IBOutlet UILabel *HZValueLabel;
+    #define SAMPLE_RATE 44100  //22050 //44100
+    #define FRAMESIZE  512
+    #define NUMCHANNELS 2
+    #define kOutputBus 0
+    #define kInputBus 1
 }
-
 @end
