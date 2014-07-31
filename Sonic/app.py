1c92cc899e2ca5befeff98e96a28885d13cf3cc4
Edwin Zhang <edwin.zhang@macboxf0fh04.corp.hulu.com>
2014-07-31 15:23:02 -0700

    Flask data

--- Sonic/app.py
+++ Sonic/app.py
@@ -1,10 +1,23 @@
 from sql_alchemy_declaritive import Question, Answer, engine, Base
 from sqlalchemy.orm import sessionmaker
 
+from flask import Flask, Response, jsonify
+
+app = Flask(__name__)
+
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 
-session = DBSession()
+@app.route('/health_check')
+def health_check():
+    return Response('OK', content_type="text/plain")
 
-
+@app.route('/question/<frequency>')
+def question(frequency):
+    session = DBSession()
+    question = session.query(Question).filter(Question.frequency > int(frequency)-2, Question.frequency < int(frequency) + 2).first()
+    question_text = question.question
+    choices = [answer.answer for answer in question.answers]
+    answer = [index for index,answer in enumerate(question.answers) if answer.correct].pop()
+    return jsonify(question=question_text, choices=choices, answer=answer)
